21c4e011d0495912a18523d7898cffbd3c2a4055
Daniel Schneider <daniel.reis.schneider@gmail.com>
2022-04-27 20:33:21 -0300

    feat: add stats

--- src/script.ts
+++ src/script.ts
@@ -3,9 +3,11 @@ import ThunderScene from "./game/ThunderScene";
 import GameCamera from "./game/GameCamera";
 import './style.css'
 
-const canvas = document.querySelector('canvas.webgl')
+const canvas: HTMLCanvasElement = document.querySelector('canvas.webgl')
 
 new Engine(canvas, ThunderScene)
     .activePhysics()
+    .activeStats()
     .setCamera(new GameCamera())
     .start()
+    
